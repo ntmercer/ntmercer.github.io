6791ce14287daf1a3e9574cd24e51313a16dc749
ntmercer <54683390+ntmercer@users.noreply.github.com>
2021-10-07 17:41:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ fetch(https://random.dog/woof.json)
       .then( (data) => data.json())
       .then( (animal) => generateHTML(animal) )
 
-const generateHTML = ( (data) => {
+const generateHTML = (data) => {
     console.log(data)
     const html = `
         <img src=${data.url}>
