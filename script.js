27ef5287d41af29ad1d47250785b2ab21c62d086
ntmercer <54683390+ntmercer@users.noreply.github.com>
2021-10-07 17:37:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ function revealMessage()
   document.getElementById("hiddenMessage").style.display = 'block';
 }
 
-fetch(https://aws.random.cat/meow)
+fetch(https://random.dog/woof.json)
       .then( (data) => data.json())
       .then( (animal) => generateHTML(animal) )
 
 const generateHTML = ( (data) => {
     console.log(data)
     const html = `
-        <img src=${data.file}>
+        <img src=${data.url}>
     `
     const catDiv = document.querySelector('.cat')
     catDiv.innerHTML = html
