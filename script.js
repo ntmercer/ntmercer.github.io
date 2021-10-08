43e8d950e1addc3e87402e0280b9dda71bb3bf22
ntmercer <54683390+ntmercer@users.noreply.github.com>
2021-10-08 00:03:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,103 @@ const generateHTML = (data) => {
     const catDiv = document.querySelector('.cat')
     catDiv.innerHTML = html
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const cardArray = [
+        {
+              name: 'dog',
+              img: 'https://random.dog/2a99e070-ac7c-444f-9906-223338c4afee.jpg'
+        },
+        {
+              name: 'dog',
+              img: 'https://random.dog/2a99e070-ac7c-444f-9906-223338c4afee.jpg'
+        },
+        {
+              name: 'dog',
+              img: 'https://random.dog/2a99e070-ac7c-444f-9906-223338c4afee.jpg'
+        },
+        {
+              name: 'dog',
+              img: 'https://random.dog/2a99e070-ac7c-444f-9906-223338c4afee.jpg'
+        },
+        {
+              name: 'dog',
+              img: 'https://random.dog/2a99e070-ac7c-444f-9906-223338c4afee.jpg'
+        },
+        {
+              name: 'dog',
+              img: 'https://random.dog/2a99e070-ac7c-444f-9906-223338c4afee.jpg'
+        },
+        {
+              name: 'dog',
+              img: 'https://random.dog/2a99e070-ac7c-444f-9906-223338c4afee.jpg'
+        },
+        {
+              name: 'dog',
+              img: 'https://random.dog/2a99e070-ac7c-444f-9906-223338c4afee.jpg'
+        },
+        {
+              name: 'dog',
+              img: 'https://random.dog/2a99e070-ac7c-444f-9906-223338c4afee.jpg'
+        },
+        {
+              name: 'dog',
+              img: 'https://random.dog/2a99e070-ac7c-444f-9906-223338c4afee.jpg'
+        },
+        {
+              name: 'dog',
+              img: 'https://random.dog/2a99e070-ac7c-444f-9906-223338c4afee.jpg'
+        },
+        {
+              name: 'dog',
+              img: 'https://random.dog/2a99e070-ac7c-444f-9906-223338c4afee.jpg'
+        },
+        {
+              name: 'dog',
+              img: 'https://random.dog/2a99e070-ac7c-444f-9906-223338c4afee.jpg'
+        },
+        {
+              name: 'dog',
+              img: 'https://random.dog/2a99e070-ac7c-444f-9906-223338c4afee.jpg'
+        },
+        {
+              name: 'dog',
+              img: 'https://random.dog/2a99e070-ac7c-444f-9906-223338c4afee.jpg'
+        },
+        {
+              name: 'dog',
+              img: 'https://random.dog/2a99e070-ac7c-444f-9906-223338c4afee.jpg'
+        },
+        {
+              name: 'dog',
+              img: 'https://random.dog/2a99e070-ac7c-444f-9906-223338c4afee.jpg'
+        },
+        {
+              name: 'dog',
+              img: 'https://random.dog/2a99e070-ac7c-444f-9906-223338c4afee.jpg'
+        },
+        {
+              name: 'dog',
+              img: 'https://random.dog/2a99e070-ac7c-444f-9906-223338c4afee.jpg'
+        },
+        {
+              name: 'dog',
+              img: 'https://random.dog/2a99e070-ac7c-444f-9906-223338c4afee.jpg'
+        }
+  ]
+  
+  const grid = document.querySelector('.grid')
+  
+  function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+              var card = document.createElement('img')
+              card.setAttribute('src', 'https://via.placeholder.com/150')
+              card.setAttribute('data-id', i)
+              //card.addEventListener('click', flipcard)
+              grid.appendChild(card)
+        }      
+  }
+      
+  createBoard()
+      
+})
