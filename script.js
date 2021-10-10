5ae75b628f48894d7722b28f3d8bdb19275d3d7c
ntmercer <54683390+ntmercer@users.noreply.github.com>
2021-10-09 23:05:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const generateHTML = (data) => {
 
 document.addEventListener('DOMContentLoaded', () => {
   const cardArray = []
-  for (let i = 0; i < 10; i++) {
+  for (let x = 0; x < 10; x++) {
         
       fetch('https://random.dog/woof.json')
             .then( (data) => data.json())
