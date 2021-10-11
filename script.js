954653b2c004261eedef0dfe2e51d72a6bb66350
Nathaniel Mercer <ntmercer@live.unc.edu>
2021-10-11 12:48:49 -0400

    final draft?

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-document.addEventListener('DOMContentLoaded', () =>
+// Nathaniel Mercer - COMP 421 Final Project - Spring 2021 semester - Turned in 10/11/2021
+
+document.addEventListener('DOMContentLoaded', () => //Run program as soon as website is loaded
 {
+      //Declare variables
+      //Grid = grid tileset which holds cards
       const grid = document.querySelector('.grid')
+      //Displays = show results to user of website
       const resultDisplay = document.querySelector('#result')
       const movesDisplay = document.querySelector('#moves')
       const factDisplay = document.querySelector('#fact')
       resultDisplay.textContent = 0
       movesDisplay.textContent = 0
+      //Arrays = store cards or card info
       var cardArray = []
       var cardsChosen = []
       var cardsChosenID = []
       var cardsWon = []
+      //API URL = stores the URL of the API used based on which creature is selected (creatureType)
       var pictureApiUrl, factApiUrl, creatureType
 
+      //Once user presses "Start," begins program
+      //Or, resets program if user chooses to "Reset"
       var creatureSelectForm = document.getElementById('creatureForm')
       creatureSelectForm.addEventListener('submit',function(event)
       {
             event.preventDefault()
-
+            //Clear all data
             resultDisplay.textContent = 0
             movesDisplay.textContent = 0
             factDisplay.textContent = ""
@@ -30,6 +39,7 @@ document.addEventListener('DOMContentLoaded', () =>
             configApi()
       })
 
+      //Determines API URLs based off user creature selection
       function configApi()
       {
             creatureType = document.getElementById("creatures").value
@@ -38,21 +48,18 @@ document.addEventListener('DOMContentLoaded', () =>
                   pictureApiUrl = 'https://dog.ceo/api/breeds/image/random'
                   factApiUrl = 'https://some-random-api.ml/facts/dog'
             }
-            else if(creatureType == 'cat')
+            else //must be cat
             {
                   pictureApiUrl = 'https://cataas.com/cat?json=true'
                   factApiUrl = 'https://meowfacts.herokuapp.com'
             }
-            else //must be koala
-            {
-                  pictureApiUrl = 'https://some-random-api.ml/animal/koala'
-                  factApiUrl = 'https://some-random-api.ml/facts/koala'
-            }
             buildCardArray()
       }
 
+      //Set up the cards for the game in an array, for future reference
       function buildCardArray()
       {
+            //Create a random order for cards to go in
             var indexes = []
             for (let x = 0; x < 20; ++x)
             {
@@ -60,7 +67,10 @@ document.addEventListener('DOMContentLoaded', () =>
             }
             indexes.sort(() => 0.5 - Math.random())
 
-            for (let x = 0; x < 10; ++x)
+            //Create cards and store them in an array based on the random order
+            //Each card remembers the URL to get its image, and whether it is currently flipped or not
+            //The URL is taken from the API, so it changes based on the animal selected by the user
+            for (let z = 0; z < 10; ++z)
             {
                   fetch(pictureApiUrl)
                         .then(response =>
@@ -69,22 +79,21 @@ document.addEventListener('DOMContentLoaded', () =>
                               })
                               .then(data => 
                               {
-                                    var newCard
+                                    let newCard
                                     if(creatureType == 'dog')
                                           newCard = {img: data.message, isFlipped: false}
-                                    else if(creatureType == 'cat')
+                                    else //must be cat
                                           newCard = {img: 'https://cataas.com' + data.url, isFlipped: false}
-                                    else
-                                          newCard = {img: data.image, isFlipped: false}
-                                    cardArray[indexes[x]] = newCard
-                                    cardArray[indexes[x+10]] = newCard
-                                    console.log(newCard)
+                                    cardArray[indexes[z]] = newCard
+                                    cardArray[indexes[z+10]] = newCard
                               })
             }
             console.log(cardArray)
             createBoard()
       }
       
+      //Creates the actual card elements to be displayed on the webpage. Each one shows only the back of the card, for now.
+      //Each card is assigned an event listener to attempt a flip when the card is clicked
       function createBoard()
       {
             for (let i = 0; i < 20; i++)
@@ -98,8 +107,33 @@ document.addEventListener('DOMContentLoaded', () =>
                   grid.appendChild(card)
             }      
       }
+
+      //Attempt a card flip. If the clicked card is already flipped over, nothing happens
+      //If the card is NOT already flipped over, it will be flipped
+      //The card's real image is loaded, and the program remembers it has been flipped
+      //Finally, if 2 cards are flipped, after giving some time for the image to be retrieved from the internet, check for a match
+      function flipCard()
+      {
+            var cardID = this.getAttribute('data-id')
+            if(!cardArray[cardID].isFlipped || (cardArray[cardID] === cardArray[cardsChosenID[0]] && cardID != cardsChosenID[0]))
+            {
+                  cardArray[cardID].isFlipped = true
+                  cardsChosen.push(cardArray[cardID].img)
+                  cardsChosenID.push(cardID)
+                  this.setAttribute('src', cardArray[cardID].img)
+                  this.setAttribute('height', 150)
+                  this.setAttribute('width', 150)
+                  if(cardsChosen.length === 2)
+                  {
+                        setTimeout(checkForMatch, 600)
+                  }
+            }
+      } 
             
-      //check for a match
+      //Checks for a match. Compares the two flipped cards; if they have the same image, they are a match
+      //If it's a match: Records the cards that have been won. They stay flipped over
+      //If it's not a match: Flips the cards back over, so the back of the card is displayed again
+      //Regardless of result, the score and moves are updated. If the player has won, a fact is retrieved from the proper URL
       function checkForMatch()
       {
             var cards = document.querySelectorAll('img')
@@ -146,24 +180,5 @@ document.addEventListener('DOMContentLoaded', () =>
                                     }
                               })
             }
-      }
-            
-      //flip card
-      function flipCard()
-      {
-            var cardID = this.getAttribute('data-id')
-            if(!cardArray[cardID].isFlipped || (cardArray[cardID] === cardArray[cardsChosenID[0]] && cardID != cardsChosenID[0]))
-            {
-                  cardArray[cardID].isFlipped = true
-                  cardsChosen.push(cardArray[cardID].img)
-                  cardsChosenID.push(cardID)
-                  this.setAttribute('src', cardArray[cardID].img)
-                  this.setAttribute('height', 150)
-                  this.setAttribute('width', 150)
-                  if(cardsChosen.length === 2)
-                  {
-                        setTimeout(checkForMatch, 500)
-                  }
-            }
-      }    
+      }   
 })
