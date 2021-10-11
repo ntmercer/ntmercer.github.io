ec23895d75d17b83c10481223fe3d3d47c27f2ff
Nathaniel Mercer <ntmercer@live.unc.edu>
2021-10-11 00:10:17 -0400

    game completed draft

--- script.js
+++ script.js
@@ -1,65 +1,98 @@
 document.addEventListener('DOMContentLoaded', () =>
 {
-      var creatureType = 'dog'
-      var pictureApiUrl, factApiUrl
-      if(creatureType == 'dog')
-      {
-            pictureApiUrl = 'https://dog.ceo/api/breeds/image/random'
-            factApiUrl = 'https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1'
-      }
-      else if(creatureType == 'cat')
-            pictureApiUrl = 'https://cataas.com/cat?json=true'
-      else
-            pictureApiUrl = 'https://axoltlapi.herokuapp.com/'
-
-      var indexes = []
-      for (let x = 0; x < 20; ++x)
-      {
-            indexes.push(x)
-      }
-      indexes.sort(() => 0.5 - Math.random())
-
-      var cardArray = []
-      for (let x = 0; x < 10; ++x)
-      {
-            fetch(pictureApiUrl)
-                  .then(response =>
-                        {
-                              return response.json()
-                        })
-                        .then(data => 
-                        {
-                              var newCard
-                              if(creatureType == 'dog')
-                                    newCard = {img: data.message, isWon: false}
-                              else if(creatureType == 'cat')
-                                    newCard = {img: 'https://cataas.com' + data.url, isWon: false}
-                              else
-                                    newCard = {img: data.url, isWon: false}
-                              cardArray[indexes[x]] = newCard
-                              cardArray[indexes[x+10]] = newCard
-                              console.log(newCard)
-                        })
-      }
-      
-      console.log(cardArray)
-      
       const grid = document.querySelector('.grid')
       const resultDisplay = document.querySelector('#result')
       const movesDisplay = document.querySelector('#moves')
       const factDisplay = document.querySelector('#fact')
       resultDisplay.textContent = 0
       movesDisplay.textContent = 0
+      var cardArray = []
       var cardsChosen = []
       var cardsChosenID = []
       var cardsWon = []
+      var pictureApiUrl, factApiUrl, creatureType
+
+      var creatureSelectForm = document.getElementById('creatureForm')
+      creatureSelectForm.addEventListener('submit',function(event)
+      {
+            event.preventDefault()
+
+            resultDisplay.textContent = 0
+            movesDisplay.textContent = 0
+            factDisplay.textContent = ""
+            cardArray = []
+            cardsChosen = []
+            cardsChosenID = []
+            cardsWon = []
+            while(grid.firstChild)
+                  grid.removeChild(grid.firstChild)
+
+            configApi()
+      })
+
+      function configApi()
+      {
+            creatureType = document.getElementById("creatures").value
+            if(creatureType == 'dog')
+            {
+                  pictureApiUrl = 'https://dog.ceo/api/breeds/image/random'
+                  factApiUrl = 'https://some-random-api.ml/facts/dog'
+            }
+            else if(creatureType == 'cat')
+            {
+                  pictureApiUrl = 'https://cataas.com/cat?json=true'
+                  factApiUrl = 'https://meowfacts.herokuapp.com'
+            }
+            else //must be koala
+            {
+                  pictureApiUrl = 'https://some-random-api.ml/animal/koala'
+                  factApiUrl = 'https://some-random-api.ml/facts/koala'
+            }
+            buildCardArray()
+      }
+
+      function buildCardArray()
+      {
+            var indexes = []
+            for (let x = 0; x < 20; ++x)
+            {
+                  indexes.push(x)
+            }
+            indexes.sort(() => 0.5 - Math.random())
+
+            for (let x = 0; x < 10; ++x)
+            {
+                  fetch(pictureApiUrl)
+                        .then(response =>
+                              {
+                                    return response.json()
+                              })
+                              .then(data => 
+                              {
+                                    var newCard
+                                    if(creatureType == 'dog')
+                                          newCard = {img: data.message, isFlipped: false}
+                                    else if(creatureType == 'cat')
+                                          newCard = {img: 'https://cataas.com' + data.url, isFlipped: false}
+                                    else
+                                          newCard = {img: data.image, isFlipped: false}
+                                    cardArray[indexes[x]] = newCard
+                                    cardArray[indexes[x+10]] = newCard
+                                    console.log(newCard)
+                              })
+            }
+            console.log(cardArray)
+            createBoard()
+      }
       
       function createBoard()
       {
             for (let i = 0; i < 20; i++)
             {
                   var card = document.createElement('img')
-                  card.setAttribute('src', 'https://via.placeholder.com/150')
+                  card.setAttribute('src', 'backofcard.jpg')
+                  card.setAttribute('height', 150)
+                  card.setAttribute('width', 150)
                   card.setAttribute('data-id', i)
                   card.addEventListener('click', flipCard)
                   grid.appendChild(card)
@@ -75,13 +108,17 @@ document.addEventListener('DOMContentLoaded', () =>
             if (cardsChosen[0] === cardsChosen[1])
             {
                   alert('You found a match')
-                  cardArray[cardsChosenID[0]].isWon = true
-                  cardArray[cardsChosenID[1]].isWon = true
                   cardsWon.push(cardsChosen)
             } else
             {
-                  cards[optionOneID].setAttribute('src', 'https://via.placeholder.com/150')
-                  cards[optionTwoID].setAttribute('src', 'https://via.placeholder.com/150')
+                  cards[optionOneID].setAttribute('src', 'backofcard.jpg')
+                  cards[optionTwoID].setAttribute('src', 'backofcard.jpg')
+                  cards[optionOneID].setAttribute('height', 150)
+                  cards[optionTwoID].setAttribute('width', 150)
+                  cards[optionOneID].setAttribute('height', 150)
+                  cards[optionTwoID].setAttribute('width', 150)
+                  cardArray[cardsChosenID[0]].isFlipped = false
+                  cardArray[cardsChosenID[1]].isFlipped = false
                   alert('Sorry, try again')
             }
             cardsChosen = []
@@ -92,15 +129,22 @@ document.addEventListener('DOMContentLoaded', () =>
             {
                   resultDisplay.textContent = 'Congratulations! You won!'
                   fetch(factApiUrl)
-                  .then(response =>
-                        {
-                              return response.json()
-                        })
-                        .then(data => 
-                        {
-                              console.log(data)
-                              factDisplay.textContent = data.fact
-                        })
+                        .then(response =>
+                              {
+                                    return response.json()
+                              })
+                              .then(data => 
+                              {
+                                    console.log(data)
+                                    if(creatureType == 'cat')
+                                    {
+                                          factDisplay.textContent = data.data[0]
+                                    }
+                                    else
+                                    {
+                                          factDisplay.textContent = data.fact
+                                    }
+                              })
             }
       }
             
@@ -108,8 +152,9 @@ document.addEventListener('DOMContentLoaded', () =>
       function flipCard()
       {
             var cardID = this.getAttribute('data-id')
-            if(!cardArray[cardID].isWon)
+            if(!cardArray[cardID].isFlipped || (cardArray[cardID] === cardArray[cardsChosenID[0]] && cardID != cardsChosenID[0]))
             {
+                  cardArray[cardID].isFlipped = true
                   cardsChosen.push(cardArray[cardID].img)
                   cardsChosenID.push(cardID)
                   this.setAttribute('src', cardArray[cardID].img)
@@ -120,8 +165,5 @@ document.addEventListener('DOMContentLoaded', () =>
                         setTimeout(checkForMatch, 500)
                   }
             }
-      }
-            
-      createBoard()
-      
+      }    
 })
